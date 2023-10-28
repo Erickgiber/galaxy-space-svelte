de99698260069fc745432e0eb871e7706bcb757c
Erick Ramírez <erickgiber7@gmail.com>
2023-10-28 03:06:55 -0400

    typed fixed

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,15 +2,16 @@
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
 import { createSupabaseLoadClient } from '@supabase/auth-helpers-sveltekit'
 import type { Database } from '../DatabaseDefinitions.js'
+import type { Load } from '@sveltejs/kit'
 
-export const load = async ({ fetch, data, depends }) => {
+export const load: Load = async ({ fetch, data, depends }) => {
 	depends('supabase:auth')
 
 	const supabase = createSupabaseLoadClient<Database>({
 		supabaseUrl: PUBLIC_SUPABASE_URL,
 		supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
 		event: { fetch },
-		serverSession: data.session
+		serverSession: data!.session
 	})
 
 	const {
