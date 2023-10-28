c21704bab96dc0ef5500d4be42f0550e85bd325e
Erick Ramírez <erickgiber7@gmail.com>
2023-10-28 03:07:03 -0400

    Fixed styles button

--- src/routes/(public)/(auth)/login/+page.svelte
+++ src/routes/(public)/(auth)/login/+page.svelte
@@ -97,9 +97,9 @@
             cursor-pointer border-2 font-medium border-indigo-600 text-white"
 		>
 			<span
-				class="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-indigo-600 top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"
+				class="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-indigo-600 top-1/2 h-64 -translate-y-32 ease"
 			/>
-			<span class="relative text-indigo-600 transition duration-300 group-hover:text-white ease"
+			<span class="relative text-indigo-600 transition duration-300 text-white ease"
 				>Sign up</span
 			>
 		</a>
