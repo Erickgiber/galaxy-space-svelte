638c0f9316eb227e63c84314fd68e94743b93973
Erick Ramirez <erickgiber7@gmail.com>
2024-01-21 19:01:00 -0400

    fixing statistics darkmode

--- src/routes/space/statistics/+page.svelte
+++ src/routes/space/statistics/+page.svelte
@@ -8,7 +8,9 @@
 	<title>Statistics | Galaxy Space</title>
 </svelte:head>
 
-<article class="flex gap-3 flex-col bg-white px-2 py-4 mt-5 rounded-xl shadow-sm">
+<article
+	class="flex gap-3 flex-col bg-white dark:bg-dark_bg dark:text-white px-2 py-4 mt-5 rounded-xl shadow-sm"
+>
 	<div class="flex flex-col px-4">
 		<span class="text-lg">Users registered in the app</span>
 		<span class="text-base font-bold"> Total: {data.totalUsers}</span>
@@ -17,7 +19,7 @@
 	{#each data.users as user}
 		<a
 			href="/space/u/{user.username}"
-			class="user-a shadow-sm border border-light_gray py-2.5 flex items-center gap-1 pl-2 pr-5 rounded-xl hover:shadow-sm"
+			class="user-a shadow-sm border bg-dark_white border-light_gray dark:border-dark_light_gray py-2.5 flex items-center gap-1 pl-2 pr-5 rounded-xl hover:shadow-sm"
 		>
 			<img
 				class="rounded-full w-12 h-12 object-cover"
