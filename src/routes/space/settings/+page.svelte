f11df90c15a0a46688c36ef14190949f1a877301
Erick Ramirez <erickgiber7@gmail.com>
2024-02-05 22:36:58 -0400

    mejoras de estilos

--- src/routes/space/settings/+page.svelte
+++ src/routes/space/settings/+page.svelte
@@ -20,20 +20,18 @@
 	<meta name="description" content="Config your Galaxy Space account" />
 </svelte:head>
 
-<article class="flex items-centerm p-2 pb-3 sm:p-0 sm:pb-1 gap-1 border-b border-gray-300 text-xl dark:border-dark_light_gray">
-	<Icon icon="solar:settings-linear" class="text-primary text-2xl" />
+<article class="flex items-center p-2 pb-3 sm:p-0 sm:pb-1 gap-1 border-b border-gray-300 text-xl dark:border-dark_light_gray">
+	<Icon icon="solar:settings-linear" class="text-primary" width={21} />
 	<h1 class="font-semibold dark:text-white">Settings</h1>
 </article>
 
 <section class="sm:p-0 p-2">
 	<button
-	on:click={toggleDarkMode}
-	type="button"
-	class="bg-white dark:text-white hover:text-white text-dark dark:bg-dark_white flex items-center gap-1 mt-5 bg-text-white px-3 py-2 rounded-xl hover:bg-dark_white active:bg-dark_white focus:outline-none shadow hover:shadow-md transition duration-50"
->
-	<Icon icon="material-symbols:dark-mode" width="20" />
-	Dark Mode
-</button>
-
+		on:click={toggleDarkMode}
+		type="button"
+		class="bg-white dark:text-white hover:text-white text-dark dark:bg-dark_white flex items-center gap-1 mt-5 bg-text-white px-3 py-2 rounded-xl hover:bg-dark_white active:bg-dark_white focus:outline-none shadow hover:shadow-md transition duration-50"
+	>
+		<Icon icon="material-symbols:dark-mode" width="20" />
+		Dark Mode
+	</button>
 </section>
-
