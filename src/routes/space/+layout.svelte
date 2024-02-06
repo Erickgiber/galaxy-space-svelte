edc1ec738058e20a13bec3a7a8ef4955afcb6058
Erick Ramirez <erickgiber7@gmail.com>
2024-02-05 22:15:44 -0400

    added loader

--- src/routes/space/+layout.svelte
+++ src/routes/space/+layout.svelte
@@ -1,12 +1,28 @@
-<script>
+<script lang="ts">
 	import { goto } from '$app/navigation'
+	import appLoader from '$lib/assets/logos/loader.png'
 	import Aside from '$lib/components/layout/Aside.svelte'
 	import Header from '$lib/components/layout/Header.svelte'
 	import { currentUser } from '$lib/store/currentUser'
+	import { onMount } from 'svelte'
+	import { fade, slide } from 'svelte/transition'
+
+	let isLoadingApp = true
 
 	$: if (!$currentUser) {
 		goto('/')
 	}
+
+	$: if (isLoadingApp && typeof window !== 'undefined') {
+		window.document.body.style.cssText = `overflow: hidden !important;`
+	}
+
+	onMount(() => {
+		setTimeout(() => {
+			isLoadingApp = false
+			window.document.body.style.cssText = `overflow: auto !important;`
+		}, 1000)
+	})
 </script>
 
 {#if $currentUser}
@@ -18,4 +34,15 @@
 			<slot />
 		</div>
 	</main>
+{/if}
+
+{#if isLoadingApp}
+	<div
+		out:fade={{ duration: 150, delay: 200 }}
+		class="flex flex-col items-center justify-center w-full h-full fixed top-0 left-0 bg-bg dark:bg-dark_bg overflow-hidden"
+	>
+		<img out:fade={{ duration: 50 }} class="min-w-[125px] min-h-[125px] max-w-[150px] max-h-[150px] w-full h-full" src={appLoader} alt="" />
+		<br />
+		<span out:slide={{ duration: 200 }} class="dark:text-white font-semibold text-xl">Galaxy Space</span>
+	</div>
 {/if}
