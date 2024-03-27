3104763140c524931e5799e836fb3a90ac929ecb
Erick Ramirez <erickgiber7@gmail.com>
2024-03-27 13:13:54 -0400

    fixed responsive

--- src/routes/space/services/+page.svelte
+++ src/routes/space/services/+page.svelte
@@ -40,20 +40,22 @@
 <BreadCrumb data={{ current: { title: 'Services', icon: 'ant-design:api-outlined' } }} />
 
 {#if !authWithAnyApp}
-	<div class="dark:text-white flex items-center gap-1.5 bg-white dark:bg-dark_light_gray p-2 rounded-lg mt-1 mb-3">
-		<Icon icon="emojione:warning" class="-translate-y-[1px]" />
-		<p>Take it easy! No private information will be saved. Nor will it be shared</p>
-	</div>
+	<section class="px-3">
+		<div class="dark:text-white flex items-center gap-1.5 bg-white dark:bg-dark_light_gray p-2 rounded-lg mt-1 mb-3">
+			<Icon icon="emojione:warning" class="-translate-y-[1px]" />
+			<p>Take it easy! No private information will be saved. Nor will it be shared</p>
+		</div>
 
-	{#if !selectServiceOpened.enable}
-		<button
-			on:click={() => setServiceOpened(true, 'github')}
-			class="bg-[#161B22] border border-[#30363D] text-white font-bold py-2 px-4 rounded
+		{#if !selectServiceOpened.enable}
+			<button
+				on:click={() => setServiceOpened(true, 'github')}
+				class="bg-[#161B22] border border-[#30363D] text-white font-bold py-2 px-4 rounded
     transition-all hover:bg-opacity-80 active:scale-95 active:transition-none"
-		>
-			Auth with GitHub
-		</button>
-	{/if}
+			>
+				Auth with GitHub
+			</button>
+		{/if}
+	</section>
 {:else}
 	<div class="flex flex-wrap px-3 sm:px-0">
 		{#each ServicesList as service}
